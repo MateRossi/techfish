e6d30f300ca984aecef46b0e4a999512ab99fb46
mateus rossi <rossijf2@gmail.com>
2024-03-27 14:37:16 -0300

    add teste.js

---
+++ teste.js
@@ -0,0 +1,14 @@
+const dadosMestre = 
+{
+    "clientId": "endereçoMACMESTRE",    //id do cliente => necessário enviar do aparelho Mestre.
+    "id": 1,                            //identifica o aparelho (conjunto de sensores) => necessário enviar.
+    "data": "27/03/2024",               //data da leitura => precisa enviar.
+    "hora": "13:35:40:900",             //hora da leitura => precisa enviar.
+    "ph": 6,                            //enviar
+    "temperatura": 23,                  //enviar
+    "orp": 2,                           //enviar
+    "tds": 32,                          //enviar
+    "o2": 43,                           //enviar
+    "o2_mg": 34,                        //enviar
+    "turbidez": 54                      //enviar
+}
