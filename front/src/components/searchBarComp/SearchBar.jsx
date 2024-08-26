7f03e360056826f7086afddaebba30334306dca6
MateRossi <rossijf2@gmail.com>
2024-08-26 16:07:28 -0300

    adicionado ícone de lupa na barra de pesquisa de tanques

--- front/src/components/searchBarComp/SearchBar.jsx
+++ front/src/components/searchBarComp/SearchBar.jsx
@@ -1,9 +1,13 @@
+import LupaIcon from "../../icons/LupaIcon";
 import "./SearchBar.css";
 
 function SearchBar({ elementToAdd }) {
     return (
         <div className="search-bar-container">
-            <input type="text" className="search-bar-input" placeholder={`Buscar ${elementToAdd}`}/>
+            <span className="search-bar-input-container">
+                <LupaIcon className="search-bar-lupe-icon" />
+                <input type="text" className="search-bar-input" placeholder={`Buscar ${elementToAdd}`} />
+            </span>
             <button className="search-bar-button">Adicionar {elementToAdd}</button>
         </div>
     );
