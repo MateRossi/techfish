d45b988f843731b46e55697c63f37bdb586589a3
MateRossi <rossijf2@gmail.com>
2024-10-07 10:04:26 -0300

    conteúdo inicial da página home

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/use-auth";
+
 export default function HomePage() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        let isMounted = true;
+
+        if (!auth?.accessToken) {
+            if (isMounted) {
+                navigate('/auth', { state: { from: location }, replace: true });
+            }
+        }
+
+        return () => isMounted = false;
+    }, [auth?.accessToken, location, navigate]);
+
     return (
         <main className="page">
             <div>teste</div>
