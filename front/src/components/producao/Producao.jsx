1b1356c38c65dcfb6954a4c5f8dee8ed070a4674
MateRossi <rossijf2@gmail.com>
2025-04-10 13:32:50 -0300

    Ajuste de CSS no item de produção

--- front/src/components/producao/Producao.jsx
+++ front/src/components/producao/Producao.jsx
@@ -6,17 +6,28 @@ export default function Producao({ producao }) {
 
     return (
         <div className="producao-item" key={producao.id}>
-            <div className='title'>
-                <h2>Tanque {producao.producao.tanque.nome}</h2>
-                <p>Especie: {producao.producao.especie.nome}</p>
+            <div className='producao-content'>
+                <div className='title'>
+                    <h2>Tanque {producao.producao.tanque.nome}</h2>
+                    <p>Especie: {producao.producao.especie.nome}</p>
+                </div>
+                <div className='production-prop-items-container'>
+                    <ProducaoPropItem title="TCA" value={producao.producao.tcaGeral || '-'} />
+                    <ProducaoPropItem title="GPD" value={producao.producao.gpdGeral || '-'} valueType="cm" />
+                    <ProducaoPropItem title="Massa atual" value={producao.producao.pesoIndividualFinal || producao.producao.pesoMedioIndividualInicial || '-'} valueType="g" />
+                    <ProducaoPropItem title="Fase Atual" value={producao.fase.nome} />
+                </div>
+                <p className='prod-status'>{producao.producao.status}</p>
             </div>
-            <div className='production-prop-items-container'>
-                <ProducaoPropItem title="TCA" value={producao.producao.tcaGeral || '-'} />
-                <ProducaoPropItem title="GPD" value={producao.producao.gpdGeral || '-'} valueType="cm" />
-                <ProducaoPropItem title="Massa atual" value={producao.producao.pesoIndividualFinal || '-'} valueType="g" />
-                <ProducaoPropItem title="Fase Atual" value={producao.fase.nome} />
+            <hr className='producao-separator' />
+            <div className='producao-item-footer'>
+                <div>
+                    <h2>Producao #{producao.id}</h2>
+                    <p>Total de ração fornecida: {producao.producao.totalRacaoFornecida || "-"}</p>
+                </div>
+                <button className='production-item-button'>Cadastrar Tratamento</button>
             </div>
-            <p className='prod-status'>{producao.producao.status}</p>
         </div>
+
     )
 }
